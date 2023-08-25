aaab4542db1431569c122e863970771ea1f6821e
Alejo Generoso Campagna <Alejocamparoso@gmail.com>
2023-08-25 00:18:51 -0300

    cambios

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const calcularCostoButton = document.getElementById('calcularCosto');
+const resultadoDiv = document.getElementById('resultado');
+
 // la lista de productos
 const productos = [
     { nombre: 'Camisetas', precio: 300 },
@@ -5,12 +8,6 @@ const productos = [
     { nombre: 'Pantalones', precio: 350 },
     // agregar más productos 
 ];
-
-const cantidadCamisetasInput = document.getElementById('cantidadCamisetas');
-const cantidadGorrasInput = document.getElementById('cantidadGorras');
-const cantidadPantalonesInput = document.getElementById('cantidadPantalones');
-const calcularCostoButton = document.getElementById('calcularCosto');
-const resultadoDiv = document.getElementById('resultado');
 
 // function calcular el costo total
 function calcularCostoTotal(productosSeleccionados) {
@@ -28,16 +25,17 @@ function mostrarResultado(costoTotal) {
 
 // manejador de evento para el botón de cálculo
 calcularCostoButton.addEventListener('click', function() {
-    const cantidadCamisetas = parseInt(cantidadCamisetasInput.value) || 0;
-    const cantidadGorras = parseInt(cantidadGorrasInput.value) || 0;
-    const cantidadPantalones = parseInt(cantidadPantalonesInput.value) || 0;
+    const productosSeleccionados = [];
 
+    for (const producto of productos) {
+        const cantidadInput = document.getElementById(`cantidad${producto.nombre}`);
+        const cantidad = parseInt(cantidadInput.value) || 0;
 
-    const productosSeleccionados = [
-        { nombre: 'Camisetas', precio: 300, cantidad: cantidadCamisetas },
-        { nombre: 'Gorras', precio: 150, cantidad: cantidadGorras },
-        { nombre: 'Pantalones', precio: 150, cantidad: cantidadPantalones },
-    ];
+        if (cantidad > 0) {
+            producto.cantidad = cantidad;
+            productosSeleccionados.push(producto);
+        }
+    }
 
     const costoTotal = calcularCostoTotal(productosSeleccionados);
     mostrarResultado(costoTotal);
