fcecbbb01fb256e92db04fd502bee0b273121855
Alejo Generoso Campagna <Alejocamparoso@gmail.com>
2023-09-11 17:58:57 -0300

    Actualizacion

--- main.js
+++ main.js
@@ -1,42 +1,81 @@
-const calcularCostoButton = document.getElementById('calcularCosto');
-const resultadoDiv = document.getElementById('resultado');
+const cards = document.querySelectorAll('.card');
+const cartItems = document.getElementById('cart-items');
+const totalCost = document.getElementById('total');
+const checkoutButton = document.getElementById('checkout');
+const clearCartButton = document.getElementById('clear-cart');
 
-// la lista de productos
-const productos = [
-    { nombre: 'Camisetas', precio: 300 },
-    { nombre: 'Gorras', precio: 150 },
-    { nombre: 'Pantalones', precio: 350 },
-    // agregar más productos 
-];
-
-// function calcular el costo total
-function calcularCostoTotal(productosSeleccionados) {
+// calcular costo total
+function calcularCostoTotal() {
     let costoTotal = 0;
-    for (const producto of productosSeleccionados) {
-        costoTotal += producto.precio * producto.cantidad;
-    }
+    const items = JSON.parse(localStorage.getItem('cart')) || [];
+
+    items.forEach(item => {
+        costoTotal += item.precio * item.cantidad;
+    });
+
     return costoTotal;
 }
 
-// resultado dom
-function mostrarResultado(costoTotal) {
-    resultadoDiv.innerHTML = `El costo total de su compra en SportsBuenosAires es de: $${costoTotal}`;
+// dark mode
+const modoOscuroButton = document.getElementById('modoOscuro');
+const body = document.body;
+
+modoOscuroButton.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+    
+    // Cambia el texto del botón según el modo actual
+    if (body.classList.contains('dark-mode')) {
+        modoOscuroButton.textContent = 'Cambiar a Modo Claro';
+    } else {
+        modoOscuroButton.textContent = 'Cambiar a Modo Oscuro';
+    }
+});
+
+
+// funcionn para mostrar el carrito
+function mostrarCarrito() {
+    cartItems.innerHTML = '';
+    const items = JSON.parse(localStorage.getItem('cart')) || [];
+
+    items.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.cantidad} ${item.nombre} - $${item.precio * item.cantidad}`;
+        cartItems.appendChild(li);
+    });
+
+    const costoTotal = calcularCostoTotal();
+    totalCost.textContent = `Total: $${costoTotal}`;
 }
 
-// manejador de evento para el botón de cálculo
-calcularCostoButton.addEventListener('click', function() {
-    const productosSeleccionados = [];
+// agregar productos al carrito
+cards.forEach(card => {
+    const addButton = card.querySelector('.add-to-cart');
+    const producto = card.getAttribute('data-product');
+    const precio = parseInt(card.querySelector('p').textContent.match(/\d+/)[0]);
 
-    for (const producto of productos) {
-        const cantidadInput = document.getElementById(`cantidad${producto.nombre}`);
-        const cantidad = parseInt(cantidadInput.value) || 0;
+    addButton.addEventListener('click', () => {
+        const items = JSON.parse(localStorage.getItem('cart')) || [];
+        const existingItem = items.find(item => item.nombre === producto);
 
-        if (cantidad > 0) {
-            producto.cantidad = cantidad;
-            productosSeleccionados.push(producto);
+        if (existingItem) {
+            existingItem.cantidad++;
+        } else {
+            items.push({ nombre: producto, precio, cantidad: 1 });
         }
-    }
 
-    const costoTotal = calcularCostoTotal(productosSeleccionados);
-    mostrarResultado(costoTotal);
+        localStorage.setItem('cart', JSON.stringify(items));
+        mostrarCarrito();
+    });
 });
+
+// vaciar carrito
+clearCartButton.addEventListener('click', () => {
+    localStorage.removeItem('cart');
+    mostrarCarrito();
+});
+
+checkoutButton.addEventListener('click', () => {
+    alert('Compra finalizada');
+});
+
+mostrarCarrito();
